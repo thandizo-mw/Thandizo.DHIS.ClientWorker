2e434df4952bf3a6827ec810666c54548301fded
Sokwani Munthali <sokwani.munthali@angledimension.com>
2020-04-25 18:32:10 +0200

    added appsettings.json and ConsumerModule under WorkerNode

---
+++ Thandizo.DHIS.ClientWorker/Modules/ConsumersModule.cs
@@ -0,0 +1,13 @@
+﻿using Autofac;
+using Thandizo.DHIS.ClientWorker.Consumers;
+
+namespace Thandizo.DHIS.ClientWorker.Modules
+{
+    public class ConsumersModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<AddPatientToDhisConsumer>();
+        }
+    }
+}
