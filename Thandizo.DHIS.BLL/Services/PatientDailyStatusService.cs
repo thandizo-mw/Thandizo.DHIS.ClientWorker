607592b1c349e13861979b10512e2ee62c56ddf8
Sokwani Munthali <sokwani.munthali@angledimension.com>
2020-05-20 21:14:23 +0200

    completed the integration of DHIS2 for PatientDailyStatusService

--- Thandizo.DHIS.BLL/Services/PatientDailyStatusService.cs
+++ Thandizo.DHIS.BLL/Services/PatientDailyStatusService.cs
@@ -1,4 +1,5 @@
 ﻿using AngleDimension.Standard.Http.HttpServices;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,6 +8,7 @@ using System.Threading.Tasks;
 using Thandizo.DAL.Models;
 using Thandizo.DataModels.General;
 using Thandizo.DataModels.Integrations;
+using Thandizo.DataModels.Integrations.Responses;
 using Thandizo.DataModels.Patients;
 
 namespace Thandizo.DHIS.BLL.Services
@@ -29,18 +31,36 @@ namespace Thandizo.DHIS.BLL.Services
 
         public async Task<OutputResponse> Post(IEnumerable<PatientDailyStatusDTO> statuses)
         {
-            //get dhis2 reference number
             var patientId = statuses.FirstOrDefault().PatientId;
-            var referenceNumber = _context.Patients.FirstOrDefault(x => x.PatientId.Equals(patientId)).ExternalReferenceNumber;
+            var patient = await _context.Patients.FirstOrDefaultAsync(x => x.PatientId.Equals(patientId));            
 
             //symptoms as data elements
             var symptoms = from ds in statuses
                            join s in _context.PatientSymptoms on ds.SymptomId equals s.SymptomId
                            select new DhisDataValue
                            {
-                               Value = "1",
+                               Value = "Yes",
                                DataElement = s.ExternalReferenceNumber
                            };
+
+            //get program details
+            var program = await _context.DhisPrograms.FirstOrDefaultAsync();
+
+            //get organisation unit (facility)
+            var organisationUnit = await _context.DhisOrganisationUnits
+                .FirstOrDefaultAsync(x => x.DistrictCode.Equals(patient.DistrictCode));
+
+            var trackedEntityInstance = new DhisTrackedEntityInstance
+            {
+                CompletedDate = DateTime.UtcNow.AddHours(2).Date,
+                EventDate = DateTime.UtcNow.AddHours(2).Date,
+                DataValues = symptoms,
+                OrgUnit = organisationUnit.DhisOrgUnitId,
+                TrackedEntityInstance = patient.ExternalReferenceNumber,
+                Program = program.DhisProgramId,
+                ProgramStage = program.DhisProgramStage,
+                Status = "COMPLETED"
+            };
 
             //post to dhis through basic authentication
             //***************************************
@@ -55,6 +75,21 @@ namespace Thandizo.DHIS.BLL.Services
                     HeaderValue = $"Basic { credentials }"
                 }
             };
+
+            var response = await HttpRequestFactory.Post($"{_dhisApiUrl}/events", trackedEntityInstance, headerFields);
+
+            //handle response from DHIS2 api
+            var dhisResponse = response.ContentAsType<DhisResponse>();
+
+            if (!dhisResponse.HttpStatus.Equals("OK"))
+            {
+                var messages = string.Empty;
+                foreach (var importSummary in dhisResponse.Response.ImportSummaries)
+                {
+                    messages = string.Join("; ", importSummary.Conflicts.Select(x => x.Value).ToArray());
+                }
+                throw new ArgumentException(messages);
+            }
 
             return new OutputResponse
             {
