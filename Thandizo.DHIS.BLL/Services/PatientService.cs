1cd594ca62bc899ab2d337e0f709422ceacf2c51
Sokwani Munthali <sokwani.munthali@angledimension.com>
2020-05-21 12:29:55 +0200

    added logic to update patient symptoms to mark as posted to dhis to avoid duplicated entries under POST method in PatientService

--- Thandizo.DHIS.BLL/Services/PatientService.cs
+++ Thandizo.DHIS.BLL/Services/PatientService.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 using Thandizo.DAL.Models;
 using Thandizo.DataModels.General;
 using Thandizo.DataModels.Integrations;
@@ -30,93 +31,96 @@ namespace Thandizo.DHIS.BLL.Services
 
         public async Task<OutputResponse> Post(long patientId)
         {
-            //get all dhis2 attribute mappings for patient
-            var attributes = await _context.DhisAttributes.Where(x => x.ModuleCode.Equals("PAT")).ToListAsync();
-
-            //1. GET PATIENT DETAILS
-            //************* START ************************************************
-            var patient = await _context.Patients.Where(x => x.PatientId.Equals(patientId))
-                .Select(x => new DhisPatientIntegrationDTO
-                {
-                    DateofBirth = x.DateOfBirth.Date,
-                    FirstName = x.FirstName,
-                    Gender = x.Gender.Equals("F") ? "female" : "male",
-                    HomeAddress = x.HomeAddress,
-                    NationalId = x.IdentificationType.ExternalReferenceNumber.Equals("NID") ? x.IdentificationNumber : "",
-                    PassportNumber = x.IdentificationType.ExternalReferenceNumber.Equals("PST") ? x.IdentificationNumber : "",
-                    LastName = x.LastName,
-                    NationalityName = x.NationalityCodeNavigation.ExternalReferenceNumber,
-                    NextOfKinFirstName = x.NextOfKinFirstName,
-                    NextOfKinLastName = x.NextOfKinLastName,
-                    NextOfKinPhoneNumber = x.NextOfKinPhoneNumber,
-                    CountryName = x.ResidenceCountryCodeNavigation.ExternalReferenceNumber,
-                    PatientAge = (int)((x.DateCreated.Subtract(x.DateOfBirth.Date).TotalDays) / 365),
-                    DistrictName = x.DistrictCodeNavigation.DistrictName,
-                    PhoneNumber = x.PhoneNumber,
-                    PhysicalAddress = x.PhysicalAddress,
-                    DistrictCode = x.DistrictCode,
-                    ExternalReferenceNumber = x.ExternalReferenceNumber
-                }).FirstOrDefaultAsync();
-
-            //this avoid duplicated entries to DHIS2
-            if (!string.IsNullOrEmpty(patient.ExternalReferenceNumber))
+            try
             {
-                return new OutputResponse
-                {
-                    IsErrorOccured = false,
-                    Message = "Already posted to DHIS2"
-                };
-            }
+                //get all dhis2 attribute mappings for patient
+                var attributes = await _context.DhisAttributes.Where(x => x.ModuleCode.Equals("PAT")).ToListAsync();
 
-            //prepare attributes for DHIS2 integration            
-            var attributeItems = new List<DhisTrackedEntityAttribute>();
-
-            foreach (var prop in patient.GetType().GetProperties())
-            {
-                string propertyValue = string.Empty;
-
-                if (prop.PropertyType == typeof(DateTime) || prop.PropertyType == typeof(DateTime?))
-                {
-                    propertyValue = string.Format("{0:yyyy-MM-dd}", prop.GetValue(patient, null));
-                }
-                else
-                {
-                    propertyValue = prop.GetValue(patient, null).ToString();
-                }
-
-                //get dhis attribute id
-                var attribute = attributes.FirstOrDefault(x => x.SourceColumnName.ToLower().Equals(prop.Name.ToLower()));
-
-                if (attribute != null && !string.IsNullOrEmpty(propertyValue))
-                {
-                    attributeItems.Add(new DhisTrackedEntityAttribute
+                //1. GET PATIENT DETAILS
+                //************* START ************************************************
+                var patient = await _context.Patients.Where(x => x.PatientId.Equals(patientId))
+                    .Select(x => new DhisPatientIntegrationDTO
                     {
-                        Attribute = attribute.DhisAttributeId,
-                        Value = propertyValue
-                    });
-                }
-            }
-            //************* END ***********************************************
+                        DateofBirth = x.DateOfBirth.Date,
+                        FirstName = x.FirstName,
+                        Gender = x.Gender.Equals("F") ? "female" : "male",
+                        HomeAddress = x.HomeAddress,
+                        NationalId = x.IdentificationType.ExternalReferenceNumber.Equals("NID") ? x.IdentificationNumber : "",
+                        PassportNumber = x.IdentificationType.ExternalReferenceNumber.Equals("PST") ? x.IdentificationNumber : "",
+                        LastName = x.LastName,
+                        NationalityName = x.NationalityCodeNavigation.ExternalReferenceNumber,
+                        NextOfKinFirstName = x.NextOfKinFirstName,
+                        NextOfKinLastName = x.NextOfKinLastName,
+                        NextOfKinPhoneNumber = x.NextOfKinPhoneNumber,
+                        CountryName = x.ResidenceCountryCodeNavigation.ExternalReferenceNumber,
+                        PatientAge = (int)((x.DateCreated.Subtract(x.DateOfBirth.Date).TotalDays) / 365),
+                        DistrictName = x.DistrictCodeNavigation.DistrictName,
+                        PhoneNumber = x.PhoneNumber,
+                        PhysicalAddress = x.PhysicalAddress,
+                        DistrictCode = x.DistrictCode,
+                        ExternalReferenceNumber = string.IsNullOrEmpty(x.ExternalReferenceNumber) ? "" : x.ExternalReferenceNumber
+                    }).FirstOrDefaultAsync();
 
-            //2. GET SYMPTOMS FOR THE PATIENT : these are represented as data elements
-            //************* START ************************************************
-            var symptoms = _context.PatientDailyStatuses.Where(x => x.PatientId == patientId)
-                    .Select(x => new DhisDataValue
+                //this avoid duplicated entries to DHIS2
+                if (!string.IsNullOrEmpty(patient.ExternalReferenceNumber))
+                {
+                    return new OutputResponse
                     {
-                        Value = "Yes",
-                        DataElement = x.Symptom.ExternalReferenceNumber
-                    });
-            //************* END ***********************************************
+                        IsErrorOccured = false,
+                        Message = "Already posted to DHIS2"
+                    };
+                }
 
-            //get program details
-            var program = await _context.DhisPrograms.FirstOrDefaultAsync();
+                //prepare attributes for DHIS2 integration            
+                var attributeItems = new List<DhisTrackedEntityAttribute>();
 
-            //get organisation unit (facility)
-            var organisationUnit = await _context.DhisOrganisationUnits
-                .FirstOrDefaultAsync(x => x.DistrictCode.Equals(patient.DistrictCode));
+                foreach (var prop in patient.GetType().GetProperties())
+                {
+                    string propertyValue = string.Empty;
 
-            //adding enrollments
-            var enrollments = new List<DhisEnrollment>(){
+                    if (prop.PropertyType == typeof(DateTime) || prop.PropertyType == typeof(DateTime?))
+                    {
+                        propertyValue = string.Format("{0:yyyy-MM-dd}", prop.GetValue(patient, null));
+                    }
+                    else
+                    {
+                        propertyValue = prop.GetValue(patient, null).ToString();
+                    }
+
+                    //get dhis attribute id
+                    var attribute = attributes.FirstOrDefault(x => x.SourceColumnName.ToLower().Equals(prop.Name.ToLower()));
+
+                    if (attribute != null && !string.IsNullOrEmpty(propertyValue))
+                    {
+                        attributeItems.Add(new DhisTrackedEntityAttribute
+                        {
+                            Attribute = attribute.DhisAttributeId,
+                            Value = propertyValue
+                        });
+                    }
+                }
+                //************* END ***********************************************
+
+                //2. GET SYMPTOMS FOR THE PATIENT : these are represented as data elements
+                //************* START ************************************************
+                var symptoms = await _context.PatientDailyStatuses.Where(x => x.PatientId == patientId
+                        && x.IsPostedToDhis == false)
+                        .Select(x => new DhisDataValue
+                        {
+                            Value = "Yes",
+                            DataElement = x.Symptom.ExternalReferenceNumber
+                        }).ToListAsync();
+                //************* END ***********************************************
+
+                //get program details
+                var program = await _context.DhisPrograms.FirstOrDefaultAsync();
+
+                //get organisation unit (facility)
+                var organisationUnit = await _context.DhisOrganisationUnits
+                    .FirstOrDefaultAsync(x => x.DistrictCode.Equals(patient.DistrictCode));
+
+                //adding enrollments
+                var enrollments = new List<DhisEnrollment>(){
                 new DhisEnrollment
                 {
                      EnrollmentDate = DateTime.UtcNow.AddHours(2).Date,
@@ -139,20 +143,20 @@ namespace Thandizo.DHIS.BLL.Services
                 }
             };
 
-            var trackedEntity = new DhisTrackedEntity()
-            {
-                Attributes = attributeItems,
-                OrgUnit = organisationUnit.DhisOrgUnitId,
-                TrackedEntityType = program.DhisTrackedEntityId,
-                Enrollments = enrollments
-            };
+                var trackedEntity = new DhisTrackedEntity()
+                {
+                    Attributes = attributeItems,
+                    OrgUnit = organisationUnit.DhisOrgUnitId,
+                    TrackedEntityType = program.DhisTrackedEntityId,
+                    Enrollments = enrollments
+                };
 
-            //post to dhis through basic authentication
-            //***************************************
-            byte[] credentialsBytes = Encoding.UTF8.GetBytes($"{_clientUserId}:{_clientPassword}");
-            var credentials = Convert.ToBase64String(credentialsBytes);
+                //post to dhis through basic authentication
+                //***************************************
+                byte[] credentialsBytes = Encoding.UTF8.GetBytes($"{_clientUserId}:{_clientPassword}");
+                var credentials = Convert.ToBase64String(credentialsBytes);
 
-            var headerFields = new List<HttpCustomHeaderField>
+                var headerFields = new List<HttpCustomHeaderField>
             {
                 new HttpCustomHeaderField
                 {
@@ -161,34 +165,61 @@ namespace Thandizo.DHIS.BLL.Services
                 }
             };
 
-            var response = await HttpRequestFactory.Post($"{_dhisApiUrl}/trackedEntityInstances",
-               trackedEntity, headerFields);
+                var response = await HttpRequestFactory.Post($"{_dhisApiUrl}/trackedEntityInstances",
+                   trackedEntity, headerFields);
 
-            //handle response from DHIS2 api
-            var dhisResponse = response.ContentAsType<DhisResponse>();
+                //handle response from DHIS2 api
+                var dhisResponse = response.ContentAsType<DhisResponse>();
 
-            if (dhisResponse.HttpStatus.Equals("OK"))
-            {
-                //update patient details
-                var patientToUpdate = await _context.Patients.FirstOrDefaultAsync(x => x.PatientId.Equals(patientId));
-                patientToUpdate.ExternalReferenceNumber = dhisResponse.Response.ImportSummaries.FirstOrDefault().Reference;
-                await _context.SaveChangesAsync();
-            }
-            else
-            {
-                var messages = string.Empty;
-                foreach (var importSummary in dhisResponse.Response.ImportSummaries)
+                if (dhisResponse.HttpStatus.Equals("OK"))
                 {
-                    messages = string.Join("; ", importSummary.Conflicts.Select(x => x.Value).ToArray());
+                    using (TransactionScope scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
+                    {
+                        //update patient details
+                        var referenceNumber = dhisResponse.Response.ImportSummaries.FirstOrDefault().Reference;
+                        var patientToUpdate = await _context.Patients.FirstOrDefaultAsync(x => x.PatientId == patientId);
+                        patientToUpdate.ExternalReferenceNumber = referenceNumber;
+
+                        //update patient symptoms
+                        var symptomsTo = await _context.PatientDailyStatuses.Where(x => x.PatientId == patientId)
+                            .Select(x => new PatientDailyStatuses
+                            {
+                                CreatedBy = x.CreatedBy,
+                                DateCreated = x.DateCreated,
+                                DateSubmitted = x.DateSubmitted,
+                                IsPostedToDhis = true,
+                                PatientId = x.PatientId,
+                                SubmissionId = x.SubmissionId,
+                                SymptomId = x.SymptomId
+                            }).ToListAsync();
+
+                        _context.PatientDailyStatuses.UpdateRange(symptomsTo);
+
+                        await _context.SaveChangesAsync();
+                        scope.Complete();
+                    }
                 }
-                throw new ArgumentException(messages);
+                else
+                {
+                    var messages = string.Empty;
+                    foreach (var importSummary in dhisResponse.Response.ImportSummaries)
+                    {
+                        messages = string.Join("; ", importSummary.Conflicts.Select(x => x.Value).ToArray());
+                    }
+                    throw new ArgumentException(messages);
+                }
+
+                return new OutputResponse
+                {
+                    IsErrorOccured = false,
+                    Message = "Posted to DHIS2 successfully"
+                };
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException(ex.Message);
             }
 
-            return new OutputResponse
-            {
-                IsErrorOccured = false,
-                Message = "Posted to DHIS2 successfully"
-            };
         }
     }
 }
